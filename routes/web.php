693b830f6ecfc760527c96656d7a07f5441a3fab
gstarcad63 <358754733@qq.com>
2020-09-24 07:51:49 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('help',  'StaticPagesController@help')->name('help');
 Route::get('about', 'StaticPagesController@about')->name('about');
 // 用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
