caa16bd16ae473fdf16a239a374133c8effa2d73
gstarcad63 <358754733@qq.com>
2020-09-24 10:22:01 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('about', 'StaticPagesController@about')->name('about');
 // 用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+// 用户会话
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
