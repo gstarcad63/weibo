c10ed52e19c3bb1ecec31f6e2be26e7297118a86
gstarcad63 <358754733@qq.com>
2020-09-28 07:58:38 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,40 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+
+    // 粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // A用户是否关注了B用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
